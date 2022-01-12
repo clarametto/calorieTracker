f21fa9de600dfc2de0d520d475b45df67fb3e3ed
bre-nda <brenda.kanale@student.moringaschool.com>
2022-01-12 09:43:56 +0300

    add models to the admin file

--- calorie/admin.py
+++ calorie/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Food,Profile,PostFood
 
 # Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ('date',)
+
+admin.site.register(Food)
+admin.site.register(Profile)
+admin.site.register(PostFood)
