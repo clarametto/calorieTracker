6d4f7547d384d0969304e6802a7ad46b402b78bc
bre-nda <brenda.kanale@student.moringaschool.com>
2022-01-12 21:25:12 +0300

    update the navbar file

--- calorie/urls.py
+++ calorie/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 	path('update_food/<str:pk>/',update_food,name='update_food'),
 	path('delete_food/<str:pk>/',delete_food,name='delete_food'),
 	path('update_profile/<int:id>',views.update_profile, name='update_profile'),
-	path('profile/',ProfilePage,name='profile'),
+	path('profile/', ProfilePage,name='profile'),
 	path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
