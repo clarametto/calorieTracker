7493e2ad112296642346be320019d2377b58b213
devseme <ian.ochenge@student.moringaschool.com>
2022-01-13 21:51:57 +0300

    modified signals

--- calorie/signals.py
+++ calorie/signals.py
@@ -1,8 +1,24 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from .models import Profile
+from django.dispatch import receiver
+
+# Create your models here.
+@receiver(post_save, sender=User)
 def create_profile(sender,instance,created,**kwargs):
     if created:
         Profile.objects.create(person_of=instance)
         print("profile created")
 post_save.connect(create_profile,sender=User)
+
+ 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+     if created:
+         Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+     instance.profile.save()
+
+
